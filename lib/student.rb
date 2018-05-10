c90dd1e42f9e365893ade3fce1479a5b6997ac0c
Mark Cruz <markchriscruz@yahoo.com>
2018-05-10 15:55:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,20 +2,39 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  # sql =  <<-SQL 
-  #   CREATE TABLE IF NOT EXISTS songs (
-  #     id INTEGER PRIMARY KEY, 
-  #     name TEXT, 
-  #     album TEXT
-  #     )
-  #     SQL
-  # DB[:conn].execute(sql) 
+
   attr_accessor :name, :grade
   attr_reader :id
   
   def initialize(name, grade)
     @name = name
     @grade = grade
+  end
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql =  <<-SQL 
+      DROP TABLE IF EXISTS students
+      SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, grade) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end
   
 end
